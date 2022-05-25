0096176e2d08b88be67413d1a7e353b11d50257c
thifa <63043848+thifa@users.noreply.github.com>
2022-05-25 22:08:18 +0700

    Update Shouter.java

--- src/main/java/Shouter.java
+++ src/main/java/Shouter.java
@@ -3,7 +3,7 @@ public class Shouter {
 
 	public void shout(String message) {
 		
-		String shoutMessage = message.toUpperCase() + '!';
+		String shoutMessage = message.toUpperCase(.) + '!';
 		
 		System.out.println(shoutMessage);
 		
